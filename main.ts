588c0312a0cdb7e4b748d183f060ba381375ce20
Juan9899 <98743364+Juan9899@users.noreply.github.com>
2022-03-16 09:24:29 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,30 @@
+function derecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    basic.pause(200)
+}
 function izquierda () {
     maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
     basic.pause(200)
 }
+let Ultimo_giro = 0
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
         maqueen.motorStop(maqueen.Motors.All)
         izquierda()
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+            maqueen.motorStop(maqueen.Motors.All)
+            derecha()
+            if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+                maqueen.motorStop(maqueen.Motors.All)
+                derecha()
+            }
+        }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
