1f10261e419b91ae73a85d891333ff1272eec26c
Juan9899 <98743364+Juan9899@users.noreply.github.com>
2022-03-21 11:58:27 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,34 @@
 function derecha () {
     maqueen.motorStop(maqueen.Motors.All)
     basic.pause(200)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
+    basic.pause(280)
+    maqueen.motorStop(maqueen.Motors.All)
 }
 function izquierda () {
     maqueen.motorStop(maqueen.Motors.All)
     basic.pause(200)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
-    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
+    basic.pause(280)
+    maqueen.motorStop(maqueen.Motors.All)
 }
 let Ultimo_giro = 0
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 7) {
         maqueen.motorStop(maqueen.Motors.All)
         izquierda()
-        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 7) {
             maqueen.motorStop(maqueen.Motors.All)
             derecha()
-            if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+            if (maqueen.Ultrasonic(PingUnit.Centimeters) < 7) {
                 maqueen.motorStop(maqueen.Motors.All)
                 derecha()
+                if (maqueen.Ultrasonic(PingUnit.Centimeters) < 7) {
+                    maqueen.motorStop(maqueen.Motors.All)
+                    derecha()
+                }
             }
         }
     } else {
